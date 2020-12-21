fcd86960ff1cbeb205598386ab95ef8b4d582bef
yirano <yirano@protonmail.ch>
2020-12-21 03:07:17 -0500

    Change script start and specify server port

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const jsonServer = require("json-server")
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 8000
 
 server.use(middlewares)
 server.use(router)
-server.listen(5000, () => {
-	console.log("JSON Server is running")
-})
+
+server.listen(port)
