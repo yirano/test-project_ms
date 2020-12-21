d95f8eaa5115dc75d8f7ad84ba443fecfcc1bc70
yirano <yirano@protonmail.ch>
2020-12-21 18:57:44 -0500

    Submenu animates on smaller devices to display

--- frontend/src/Components/Header/SubMenu.jsx
+++ frontend/src/Components/Header/SubMenu.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 const SubMenu = (props) => {
     const { subMenu } = props
+    const [showSubMenu, setShowSubMenu] = useState(false)
     return (
-        <div className="subMenu">
-            <div className="subMenuPrompt">
-                <FontAwesomeIcon icon={faUserCircle} size="3x" style={{ margin: '0.7rem 0' }} />
+        <div className="subMenu" onClick={() => setShowSubMenu(!showSubMenu)}>
+            <div className="subMenuPrompt" >
+                <FontAwesomeIcon icon={faUserCircle} size="3x" style={{ margin: '0.7rem' }} className="subMenuIcon" />
                 <p>{subMenu.question}</p>
             </div>
-            <div className="subMenuLinks">
+            <div className={showSubMenu ? "subMenuLinks showSubMenu" : "subMenuLinks hideSubMenu"}>
                 {subMenu.links.map(menuItem => <a href={menuItem.url}>{menuItem.text}</a>)}
-
             </div>
         </div>
     )
